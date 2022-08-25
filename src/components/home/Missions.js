ad64e417e67c933903365379c69349a18bbfd703
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-25 16:39:48 +0500

    Update Styling for Mission component

--- src/components/home/Missions.js
+++ src/components/home/Missions.js
@@ -1,27 +1,42 @@
 import React, { useEffect } from 'react';
+import { Table } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMissions } from '../../redux/actions/index';
 import MissionsItem from './MissionsItem';
 
 function Missions() {
   const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
 
   useEffect(() => {
-    dispatch(getMissions());
+    if (missions.length === 0) {
+      dispatch(getMissions());
+    }
   }, []);
-  const missions = useSelector((state) => state.missions);
 
   return (
     <div>
-      {missions.map((mission) => (
-        <MissionsItem
-          key={mission.id}
-          id={mission.id}
-          name={mission.rocketName}
-          description={mission.description}
-          reserved={mission.reserved}
-        />
-      ))}
+      <Table responsive="md" striped border hover>
+        <thead>
+          <tr className="border">
+            <th className="border">Mission</th>
+            <th className="border">Description</th>
+            <th className="border">Status</th>
+            <th className="border">.</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <MissionsItem
+              key={mission.id}
+              id={mission.id}
+              name={mission.rocketName}
+              description={mission.description}
+              reserved={mission.reserved}
+            />
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
--- src/components/home/MissionsItem.js
+++ src/components/home/MissionsItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { Button, Badge } from 'react-bootstrap';
 import { missionsActions } from '../../redux/actions';
 
 const MissionsItem = (props) => {
@@ -15,13 +16,31 @@ const MissionsItem = (props) => {
   const buttonHandler = () => {
     dispatch(missionsActions(id));
   };
+
   return (
-    <div key={id}>
-      <h2>{ name }</h2>
-      <p>{ description }</p>
-      <button type="button" onClick={buttonHandler}>{buttonName}</button>
-      <p>{ statusMission }</p>
-    </div>
+    <tr key={id}>
+      <td className="align-middle border">
+        <h3>{ name }</h3>
+      </td>
+      <td className="align-middle border">
+        <p>{ description }</p>
+      </td>
+      <td className="align-middle border">
+        <Badge bg={reserved ? 'primary' : 'secondary'} className="p-2 align-middle">
+          { statusMission }
+        </Badge>
+      </td>
+      <td className="align-middle border p-4">
+        <Button
+          type="button"
+          onClick={buttonHandler}
+          variant={reserved ? 'outline-danger' : 'outline-secondary'}
+          style={{ minWidth: '150px' }}
+        >
+          {buttonName}
+        </Button>
+      </td>
+    </tr>
   );
 };
 
